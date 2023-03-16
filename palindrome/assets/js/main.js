42660f2f294a38471ff1191da1d7e9d558710c71
LookasWasTaken~ <piacen.luca@gmail.com>
2023-03-16 14:44:56 +0100

    change and add folders

--- palindrome/assets/js/main.js
+++ palindrome/assets/js/main.js
@@ -36,9 +36,3 @@ console.log(result);
 
 const onScreen = document.querySelector("h1");
 onScreen.insertAdjacentHTML("beforeend", result);
-
-// ########################################################
-
-/* Pari e Dispari
-L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
-
